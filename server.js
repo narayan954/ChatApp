ecfe8f76b7676449c8ed11635c68747f4df3f312
Narayan soni <narayansoni854@gmail.com>
2023-03-10 17:18:16 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const Express = require('express');
+
+const app = Express()
+const http = require('http').createServer(app)
+const PORT = process.env.PORT || 3000
+
+http.listen(PORT,()=>{
+    console.log(`Listening on PORT: ${PORT}`)
+})
+
+app.use(Express.static(__dirname+"/public"))
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+"/index.html")
+})
+
+//socket
+const io = require('socket.io')(http)
+
+io.on('connection',(socket)=>{
+    console.log('connected')
+    socket.on('message',(msg)=>{
+        socket.broadcast.emit('message',msg)
+    })
+})
